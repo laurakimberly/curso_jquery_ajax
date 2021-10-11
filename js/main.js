8c951fb8d8aa9d43fabfb07c0c796fbf8bab1285
laura kimberly <laurapalhares2007@gmail.com>
2021-10-11 13:58:12 -0300

    adicionar

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function consultaCEP() {
+  $('.barra-progresso').show()
+  var cep = document.getElementById('cep').value
+  var url = 'https://viacep.com.br/ws/' + cep + '/json/'
+  console.log(url)
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function (response) {
+      console.log(response)
+      $('#logradouro').html(response.logradouro)
+      $('#uf').html(response.uf)
+      $('#localidade').html(response.localidade)
+      $('#bairro').html(response.bairro)
+      $('#titulo_cep').html('CEP ' + response.cep)
+      $('.cep').show()
+      $('.barra-progresso').hide()
+      //$("#logradouro").html(response.logradouro);
+      //document.getElementById('logradouro').innerHTML = response.logradouro
+
+      //alert(response.legradouro);
+    }
+  })
+}
+
+$(function () {
+  $('.cep').hide()
+  $('.barra-progresso').hide()
+})
